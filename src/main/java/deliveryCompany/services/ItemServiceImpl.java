533e3eedab352cec5c86019a37c124c2a5c2dbed
Eriife-git <eriifefashonista22@gmail.com>
2025-09-03 18:08:30 +0100

    delivery project

--- src/main/java/deliveryCompany/services/ItemServiceImpl.java
+++ src/main/java/deliveryCompany/services/ItemServiceImpl.java
@@ -11,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -53,7 +52,6 @@ public class ItemServiceImpl implements ItemService {
             infos.getInfos().add(trackingInfo);
 
             infos.setPackageId(items.save(item).getId());
-            log.info("Id is {}", item.getId());
             item.setTrackingInfos(trackingInfosRepo.save(infos));
             items.save(item);
 
